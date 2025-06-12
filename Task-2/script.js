dbdf0e632c5f0d564befba255d4827b21aa3f31e
Prakhar Shukla <therockstarop9956@gmail.com>
2025-06-12 10:37:54 +0530

    Task-2

---
+++ Task-2/script.js
@@ -0,0 +1,15 @@
+const addNoteBtn = document.getElementById("addNoteBtn");
+const notesContainer = document.getElementById("notesContainer");
+
+addNoteBtn.addEventListener("click", () => {
+    const note = document.createElement("div");
+    note.classList.add("note");
+
+    note.innerHTML = `
+    <textarea rows="5" placeholder="Write your note here..."></textarea>`;
+    note.addEventListener("dblclick", () => {
+        note.remove();
+    });
+
+    notesContainer.prepend(note);
+});
